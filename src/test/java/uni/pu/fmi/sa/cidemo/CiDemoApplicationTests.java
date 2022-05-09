d29cbe2449495f0bb51e3b9002ac741a703c1f45
enginmustafa <46629003+enginmustafa@users.noreply.github.com>
2022-05-09 21:18:31 +0300

    init project

---
+++ src/test/java/uni/pu/fmi/sa/cidemo/CiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.pu.fmi.sa.cidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
